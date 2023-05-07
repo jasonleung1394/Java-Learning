8a3986e90b2540304109dacdea39b4a9edb39412
jasonLeung <jasonleung1394@gmail.com>
2023-05-07 23:37:24 +1000

    init

---
+++ DesignPattern_Example/src/DesignPattern_Template.java
@@ -0,0 +1,9 @@
+public class DesignPattern_Template {
+    String pattern_name;
+    String pattern_description;
+
+
+    public String getPatternInfo (){
+        return "The pattern is \"" + pattern_name + "\" Description: " + pattern_description;
+    }
+}
